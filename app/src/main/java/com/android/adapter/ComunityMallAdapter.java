1b096f4df982a9421a3ca8f7a99d946d31e75ce9
junqinliu <275063613@qq.com>
2016-12-21 23:00:28 +0800

    社区商城界面九宫格实现

---
+++ app/src/main/java/com/android/adapter/ComunityMallAdapter.java
@@ -0,0 +1,77 @@
+package com.android.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.model.LifeItemBean;
+import com.android.qrcodeclient.R;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by liujunqin on 2016/5/12.
+ */
+public class ComunityMallAdapter extends BaseAdapter {
+
+    Context context;
+    List<LifeItemBean> list;
+
+    public ComunityMallAdapter(Context context, List<LifeItemBean> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public List getList() {
+        return list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.item_life, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Glide.with(context).load(list.get(i).getItemIcon()).into(holder.itemIcon);
+        holder.itemName.setText(list.get(i).getItemName());
+        return convertView;
+    }
+
+    static class ViewHolder {
+
+        @Bind(R.id.item_icon)
+        ImageView itemIcon;
+        @Bind(R.id.item_name)
+        TextView itemName;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/android/qrcodeclient/Personal/ComunityMallActivity.java
+++ app/src/main/java/com/android/qrcodeclient/Personal/ComunityMallActivity.java
@@ -4,28 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
 
+import com.android.adapter.ComunityMallAdapter;
+import com.android.adapter.LifeAdapter;
 import com.android.base.BaseAppCompatActivity;
+import com.android.model.LifeItemBean;
 import com.android.qrcodeclient.R;
 import com.android.utils.TextUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 
 /**
  * Created by liujunfeng on 2016/9/18.
  */
-public class ComunityMallActivity extends BaseAppCompatActivity implements View.OnClickListener{
+public class ComunityMallActivity extends BaseAppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
     @Bind(R.id.title)
     TextView title;
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    @Bind(R.id.grid_life)
+    GridView gridLife;
+
+    List<LifeItemBean> list;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_micro_card);
+        setContentView(R.layout.comunity_mall);
     }
 
     @Override
@@ -41,15 +54,41 @@ public class ComunityMallActivity extends BaseAppCompatActivity implements View.
         if (!TextUtil.isEmpty(titleName)) {
             title.setText(titleName);
         }
+
+        list = new ArrayList<>();
+        list.add(new LifeItemBean("微购", R.mipmap.weigou, null));
+        list.add(new LifeItemBean("逛街吧", R.mipmap.guangjieba, null));
+        list.add(new LifeItemBean("蘑菇街", R.mipmap.mogujie, null));
+        list.add(new LifeItemBean("微卖", R.mipmap.weimai, null));
+        list.add(new LifeItemBean("喵喵微店", R.mipmap.miaomiaoweidian, null));
+        list.add(new LifeItemBean("拍拍小店", R.mipmap.paipai, null));
+        list.add(new LifeItemBean("门店招商", R.mipmap.addshop, null));
+        list.add(new LifeItemBean("门店招商", R.mipmap.addshop, null));
+        list.add(new LifeItemBean("门店招商", R.mipmap.addshop, null));
+        gridLife.setAdapter(new ComunityMallAdapter(this, list));
     }
 
     @Override
     public void setListener() {
         toolbar.setNavigationOnClickListener(this);
+        gridLife.setOnItemClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         finish();
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Class cls = list.get(position).getCls();
+        if (cls != null) {
+            Bundle bundle = new Bundle();
+            bundle.putString(getResources().getString(R.string.develop_title), list.get(position).getItemName());
+            goNext(cls, bundle);
+        } else {
+            showToast("暂未开放");
+        }
     }
 }
