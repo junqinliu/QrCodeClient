fe4d57983355440b6b9a35b9f79c10007033be85
junqinliu <275063613@qq.com>
2017-01-12 20:20:36 +0800

    极光推送进行优化

---
+++ app/src/main/java/com/android/qrcodeclient/Life/MyReceiver.java
@@ -0,0 +1,25 @@
+package com.android.qrcodeclient.Life;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by liujunqin on 2017/1/12.
+ */
+public class MyReceiver  extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if("cn.jpush.android.intent.NOTIFICATION_OPENED".equals(intent.getAction())){
+
+
+            Intent intent1 = new Intent(context,MessageActivity.class);
+            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent1);
+
+        }
+
+    }
+}
